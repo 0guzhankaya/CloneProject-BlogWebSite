ebb9ce872e6e2c1f96c6db682bf402f9955a430d
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-07-22 21:50:10 +0300

    Dashboard dinamik hale getirildi.

--- ProgrammersBlog.Mvc/obj/Debug/net5.0/ProgrammersBlog.Mvc.AssemblyInfo.cs
+++ ProgrammersBlog.Mvc/obj/Debug/net5.0/ProgrammersBlog.Mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aecd022301d511db80ac2f8f507eff4d9f8d2c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProgrammersBlog.Services/obj/Debug/net5.0/ProgrammersBlog.Services.AssemblyInfo.cs
+++ ProgrammersBlog.Services/obj/Debug/net5.0/ProgrammersBlog.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammersBlog.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aecd022301d511db80ac2f8f507eff4d9f8d2c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammersBlog.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammersBlog.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
