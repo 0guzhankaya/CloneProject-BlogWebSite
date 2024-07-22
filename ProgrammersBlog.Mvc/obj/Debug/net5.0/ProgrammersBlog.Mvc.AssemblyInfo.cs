121e725c97a748c6a12989bb1f4772615fc8a251
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-07-22 17:54:00 +0300

    Refactor tamamlandı ve ComingSoon eklendi.

--- ProgrammersBlog.Mvc/obj/Debug/net5.0/ProgrammersBlog.Mvc.AssemblyInfo.cs
+++ ProgrammersBlog.Mvc/obj/Debug/net5.0/ProgrammersBlog.Mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511f4388bc51f8e65cd6e8d97ae58d12b02e66f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammersBlog.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
